7c1685d46fa9029585095a5d17da93cbfee9be32
zl90 <101400977+zl90@users.noreply.github.com>
2022-07-26 14:01:16 +1000

    Add routes

--- routes/articles.js
+++ routes/articles.js
@@ -1,9 +1,30 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const articleController = require("../controllers/articleController");
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+////////////////////////////// Article List ///////////////////////////////////
+router.get("/", articleController.articles_get);
+router.post("/", articleController.articles_post);
+router.put("/", articleController.articles_put);
+router.delete("/", articleController.articles_delete);
+
+/////////////////////////// Individual Article ////////////////////////////////
+router.get("/:articleId", articleController.article_get);
+router.put("/:articleId", articleController.article_put);
+router.delete("/:articleId", articleController.article_delete);
+
+////////////////////////////// Comment List ///////////////////////////////////
+router.get("/:articleId/comments", articleController.comments_get);
+router.post("/:articleId/comments", articleController.comments_post);
+router.put("/:articleId/comments", articleController.comments_put);
+router.delete("/:articleId/comments", articleController.comments_delete);
+
+/////////////////////////// Individual Comments ///////////////////////////////
+router.get("/:articleId/comments/:commentId", articleController.comment_get);
+router.put("/:articleId/comments/:commentId", articleController.comment_put);
+router.delete(
+  "/:articleId/comments/:commentId",
+  articleController.comment_delete
+);
 
 module.exports = router;
